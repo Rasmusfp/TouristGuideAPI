129d618c8502535e1d0b16f2f966b8a3db879bbe
SebastianH80 <sehi1000@stud.ek.dk>
2026-08-27 13:57:57 +0200

    Attraction update

--- src/main/java/com/example/touristguideapi/model/TouristAttraction.java
+++ src/main/java/com/example/touristguideapi/model/TouristAttraction.java
@@ -1,4 +1,28 @@
 package com.example.touristguideapi.model;
 
 public class TouristAttraction {
+    private String name;
+    private String description;
+
+
+    public TouristAttraction(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName(String name){
+        return name;
+    }
+
+    public String getDescription(String description){
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
