cc916076cc8bab25c0ef1edb0317188fa0b61a62
Rasmusfp <rap01001@stud.ek.dk>
2026-08-27 14:47:28 +0200

    Struktur

--- src/main/java/com/example/touristguideapi/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -1,4 +1,41 @@
 package com.example.touristguideapi.controller;
 
+import com.example.touristguideapi.model.TouristAttraction;
+import com.example.touristguideapi.service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("attraction")
+
 public class TouristController {
+
+    private TouristService touristService;
+
+    public class TouristController(TouristService touristService) {
+    this.touristService = touristService;
+    }
+
+    @GetMapping()
+    public ResponceEntity<ArrayList<TouristAttraction>> getAllAttractions() {
+
+        return new ResponceEntity<>(touristService.getAllAttractions(), HttpStatus.OK);
+    }
+
+    @GetMapping("{name}")
+    public ResponceEntity<ArrayList<TouristAttraction>> getAttractionByName(@PathVariable String name) {
+        TouristAttraction attraction = touristService.findMatchingAttractionByName(name);
+
+        if (attraction != null) {
+            return new ResponceEntity<>(attraction, HttpStatus.OK);
+        }
+
+        return new ResponceEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
