8a030d5ac70f9fcc956baf7c5b424dd396300003
Heescheo <Heescheo@gmail.com>
2026-09-09 17:56:27 +0200

    Tilføjet en streng til hver ENUM-værdi kaldet "tag" der bliver brugt i tags.html

--- src/main/java/com/example/touristguideapi/model/Category.java
+++ src/main/java/com/example/touristguideapi/model/Category.java
@@ -1,10 +1,23 @@
 package com.example.touristguideapi.model;
 
 public enum Category {
-    ART,
-    MUSEUM,
-    CHILD_FRIENDLY,
-    AMUSEMENT,
-    NATURE,
-    FREE
+    ART ("Kunst"),
+    MUSEUM ("Museum"),
+    CHILD_FRIENDLY ("Børnevenlig"),
+    AMUSEMENT ("Forlystelse"),
+    NATURE ("Natur"),
+    FREE ("Gratis");
+
+    //Initialiserer en string til brug i konstruktøren
+    private final String tag;
+
+    //Konstruktør der tager imod string tag
+    Category(String tag){
+        this.tag = tag;
+    }
+
+    //Public getter, der bliver kaldt i tags.html så den returnerer strengen der passer til ENUM-værdien i stedet for at returmere ENUM-værdien
+    public String getTag(){
+        return tag;
+    }
 }
